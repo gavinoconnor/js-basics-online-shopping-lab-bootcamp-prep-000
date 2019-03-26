2767c4d4ecac9c6e683ab4b2eb5820c64c08deb2
Gavin O'Connor <gedmund@gmail.com>
2019-03-26 16:01:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -56,9 +56,9 @@ function removeFromCart(item) {
     if (cart[i].itemName === item) {
       cart.splice(i, 1)
       return cart
+      }
     }
     return `That item is not in your cart.`
-    }
   }
 
 function placeOrder(cardNumber) {
